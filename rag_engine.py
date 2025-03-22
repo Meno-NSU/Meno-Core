9ada439d57a79dacebc838ef42fbf23cfd0e2d83
sckwo <sckwokyboom@gmail.com>
2025-03-22 22:24:33 +0700

    TOP_K=30

--- rag_engine.py
+++ rag_engine.py
@@ -17,7 +17,7 @@ from lightrag.lightrag.utils import EmbeddingFunc
 
 TEMPERATURE = 0.3
 QUERY_MAX_TOKENS = 8000
-TOP_K = 60
+TOP_K = 30
 WORKING_DIR = settings.working_dir
 print(f'os.path.isdir({WORKING_DIR}) = {os.path.isdir(WORKING_DIR)}')
 ABBREVIATIONS_FNAME = settings.abbreviations_file
