5a926a48a8cbbc95514c393db2fcbdb69b10691d
ruslan <ruslanone57@gmail.com>
2025-10-31 13:41:22 +0700

    Add: backend dto class

---
+++ logdb/backend_dto.py
@@ -0,0 +1,10 @@
+from message import Message
+
+class BackEndDTO:
+    def init(self) -> None:
+        self.session_start_time: int | None = None
+        self.session_end_time: int | None = None
+        self.messages: list[Message] = []
+        
+    def add_message(self, message: Message) -> None:
+        self.messages.append(message)
--- logdb/message.py
+++ logdb/message.py
@@ -1,10 +1,18 @@
-from message import Message
-
-class BackEndDTO:
+class Message:
     def init(self) -> None:
-        self.session_start_time: int | None = None
-        self.session_end_time: int | None = None
-        self.messages: list[Message] = []
-        
-    def add_message(self, message: Message) -> None:
-        self.messages.append(message)
+        self.question: str | None = None
+        self.expanded_question: str | None = None
+        self.coref_resolved_question: str | None = None
+        self.model_answer : str | None = None
+
+    def set_question(self, question: str) -> None:
+        self.question = question
+
+    def set_expanded_question(self, question: str) -> None:
+        self.expanded_question = question
+
+    def set_coref_resolved_question(self, question: str) -> None:
+        self.coref_resolved_question = question    
+
+    def set_model_answer(self, answer: str) -> None:
+        self.model_answer = answer
