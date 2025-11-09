d221c21034dbf073086a38a4d90829729ba314a6
ruslan <ruslanone57@gmail.com>
2025-11-09 15:09:36 +0700

    Add: db_models(sqlalchemy)

---
+++ logdb/db_models/db_models.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker, relationship
+from sqlalchemy import String, create_engine
+from datetime import datetime
+from sqlalchemy import DateTime, Text, UUID, Integer
+import uuid
+
+
+class Turn(DeclarativeBase):
+    __tablename__ = "turns"
+
+    conversation_id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+
+    turn: Mapped[int] = mapped_column(Integer, primary_key=True)
+    
+    original_question: Mapped[str] = mapped_column(Text) 
+    expanded_question: Mapped[str] = mapped_column(Text) 
+    resolved_question: Mapped[str] = mapped_column(Text)
+
+    conversation: Mapped["Conversation"] = relationship("Conversation", back_populates="turns")
+
+
+class Conversation(DeclarativeBase):
+    __tablename__ = "conversations"
+
+    conversation_id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+
+    user_id: Mapped[str] = mapped_column(String)
+
+    start_time: Mapped[datetime] = mapped_column(DateTime)
+    end_time: Mapped[datetime] = mapped_column(DateTime)
+ 
+    turns: Mapped[list["Turn"]] = relationship("Turn", back_populates="conversation")
+
+
+    
