5e7e31cb9bc8897771348a5a81e504c9e127ad68
ruslan <ruslanone57@gmail.com>
2025-11-09 18:18:00 +0700

    FIx: import

--- logdb/db_tables_creation.py
+++ logdb/db_tables_creation.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 from sqlalchemy import String, create_engine
 from datetime import datetime
 from sqlalchemy import DateTime, Text
-from logdb.db_models.db_models import Base
+from db_models.db_models import Base
 
 db_url = 'postgresql://logdb:123@localhost:5432/lodb'
 
