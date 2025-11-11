15edd2019715ece1780e5ff0e0c2b19d4bb25a78
sckwo <sckwokyboom@gmail.com>
2025-11-11 22:28:06 +0700

    Remove local files only flag

--- rag_engine.py
+++ rag_engine.py
@@ -379,14 +379,12 @@ async def initialize_rag() -> LightRAG:
         logger.info(f"Loading tokenizer and embedder model: {LOCAL_EMBEDDER_PATH}...")
         emb_tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained(
             LOCAL_EMBEDDER_PATH,
-            local_files_only=True
         )
         emb_model: AutoModel = AutoModel.from_pretrained(
             LOCAL_EMBEDDER_PATH,
             trust_remote_code=True,
             # device_map='cuda:0'
             device_map='cpu',
-            local_files_only=True
         )
         emb_model.eval()
         logger.info(f"Model {LOCAL_EMBEDDER_PATH} loaded successfully")
@@ -433,7 +431,7 @@ async def initialize_rag() -> LightRAG:
         await rag.initialize_storages()
         logger.info("RAG system initialized successfully")
         token_cls = AutoModelForTokenClassification.from_pretrained(
-            LOCAL_EMBEDDER_PATH, trust_remote_code=True, device_map='cpu', local_files_only=True
+            LOCAL_EMBEDDER_PATH, trust_remote_code=True, device_map='cpu'
         )
         token_cls.eval()
         embedder: GTEEmbedding = GTEEmbedding(emb_tokenizer, token_cls, normalized=True)
