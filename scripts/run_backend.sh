e100eecdb204e99b258e3a84e9da20d5e433e2c0
sckwoky <sckwokyboom@gmail.com>
2025-11-24 03:36:51 +0700

    Update run_backend.sh to change host to 0.0.0.0 and port to 9006 for backend service

--- scripts/run_backend.sh
+++ scripts/run_backend.sh
@@ -1,4 +1,4 @@
 cat backend-process.txt | xargs kill -TERM
 sleep 3
-nohup uv run uvicorn meno_core.api.main:app --host 127.0.0.1 --port 8888 > backend.log 2>&1 &
+nohup uv run uvicorn meno_core.api.main:app --host 0.0.0.0 --port 9006 > backend.log 2>&1 &
 echo $! > backend-process.txt
